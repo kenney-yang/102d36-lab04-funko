eabd29877b784ef3d5541dad4e941b221282cd78
KENNEY <kenney.yang@gmail.com>
2022-02-24 21:14:56 -0500

    added JS file to website

---
+++ app3.js
@@ -0,0 +1,16 @@
+function addTWoNumbers(num1, num2){
+   ..// let num1 =1;
+   ..// let num2 =2;
+
+    let total = num1 + num2;
+    console.log("total: ", total);
+
+}
+
+
+addTWoNumbers(1, 2);
+addTWoNumbers(10, 22);
+addTWoNumbers(3, -6);
+addTWoNumbers(2000, 4);
+
+
