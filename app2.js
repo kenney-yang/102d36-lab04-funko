6ac0a7d137987765fa6cf36e07caa80200e166db
KENNEY <kenney.yang@gmail.com>
2022-02-24 21:23:11 -0500

    Updated files to website

--- app2.js
+++ app2.js
@@ -1,3 +1,15 @@
+// console.log('hello from App2.js!');
+
+// let spacePirateImg = "https://starwarsblog.starwars.com/wp-content/uploads/2017/03/darth-vader-1-cover-header.jpg";
+
+// let userResponse = prompt('Press ok if you own a Funko Pop?');
+
+// if (userResponse == "yes") {
+//     document.write('<img src="' + spacePirateImg + '">');
+// } else {
+//     document.write("Code 102 Rocks!!") 
+// }
+
 console.log('hello from App2.js!');
 
 let spacePirateImg = "https://starwarsblog.starwars.com/wp-content/uploads/2017/03/darth-vader-1-cover-header.jpg";
